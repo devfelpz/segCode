f59b09865c25fb811a79b1127cce2e211e26ace2
Felipe Araújo <felipe.araujo@tarraf.com.br>
2024-12-05 11:34:06 -0300

    novo layout pagina de login

--- src/components/loginPage.tsx
+++ src/components/loginPage.tsx
@@ -1,77 +1,57 @@
-import { LockClosedIcon, UserIcon } from '@heroicons/react/24/solid';
 import illustration from "./../assets/illustration.svg";
-import mainLogo from "./../assets/mainLogo.svg";
+import logo from './../assets/mainLogo.svg';
+import InputField from "./input";
 
 const LoginPage = () => {
   return (
-<main className="bg-stone-800 text-white w-full h-screen flex flex-col overflow-y-auto">
-  <header>
-    <div className="flex mx-7 my-7">
-      <img src={mainLogo} alt="Logo da SegCode" className="w-60" />
-    </div>
-  </header>
-  <section className="flex-grow grid grid-cols-1 md:grid-cols-2">
-    <div className="flex justify-center items-center px-4 md:px-0">
-      <form className="w-full max-w-md px-4 sm:px-8 md:px-0" aria-labelledby="login-form-title">
-        <div className="flex flex-col items-center justify-center">
-          <p id="login-form-title" className="text-2xl sm:text-3xl md:text-4xl font-bold tracking-wide text-white">
-            SEGCODE
-          </p>
-          <p className="text-xs sm:text-sm ml-0 sm:ml-20 uppercase text-gray-400 mt-2 tracking-widest">
-            SURVEILLANCE TOOLS
-          </p>
-        </div>
-        <div className="mx-4 my-6">
-          <label htmlFor="username">Usuário</label>
-          <div className="mt-2 flex bg-stone-700">
-            <input
-              id="username"
-              name="username"
-              className="peer w-full rounded-r-md border border-gray-300 px-4 py-2 text-gray-300 outline-none transition-colors duration-300 focus:border-blue-700"
+<main className="bg-blue-100 text-white w-full h-screen flex items-center justify-center">
+
+  <div className="bg-white rounded-lg shadow-2xl flex items-center justify-center p-10 w-4/5 max-w-6xl h-4/6">
+    <div className="grid grid-cols-1 md:grid-cols-2 gap-10 w-full items-center">
+      <div className="flex justify-center items-center align">
+        <img
+          src={illustration}
+          className="sm:w-2/3 md:w-auto"
+          alt="Ilustração de ferramentas de vigilância"
+        />
+      </div>
+
+      <div className="p-8 w-full max-w-md transform">
+        <form className="space-y-6" aria-labelledby="login-form-title">
+          <div className="flex justify-center items-center align">
+             <img
+                src={logo}
+                className="max-w-full sm:w-2/3 md:w-auto"
+                alt="SEGCODE Surveillance Tools"
+              />
+          </div>
+          <div className='px-6 py-4 flex flex-col justify-center gap-8'>
+          <InputField
+              id="usuario"
+              label="Usuário"
               type="text"
-              placeholder="usuário"
-              aria-label="Nome de usuário"
             />
-            <span className="order-first rounded-l-md bg-gray-400 p-3 transition-colors duration-300 peer-focus:bg-blue-600 peer-focus:text-white">
-              <UserIcon className="size-6" />
-            </span>
-          </div>
-        </div>
-        <div className="mx-4 my-6">
-          <label htmlFor="password">Senha</label>
-          <div className="mt-2 flex bg-stone-700">
-            <input
-              id="password"
-              name="password"
-              className="peer w-full rounded-r-md border border-gray-300 px-4 py-2 text-gray-300 outline-none transition-colors duration-300 focus:border-blue-700"
+             <InputField
+              id="senha"
+              label="Senha"
               type="password"
-              placeholder="senha"
-              aria-label="Senha"
             />
-            <span className="order-first rounded-l-md bg-gray-400 p-3 transition-colors duration-300 peer-focus:bg-blue-600 peer-focus:text-white">
-              <LockClosedIcon className="size-6 font-bold" />
-            </span>
           </div>
-        </div>
-        <button
-          className="px-4 sm:px-6 py-2 sm:py-3 mt-6 bg-gradient-to-r from-blue-500 to-blue-700 rounded-full font-bold transition-transform transform-gpu hover:-translate-y-1 hover:shadow-lg w-full"
-          type="submit"
-          aria-label="Entrar no sistema SegCode"
-        >
-          Entrar
-        </button>
-      </form>
+           
+           
+          <button
+            type="submit"
+            className="w-full py-3 bg-gradient-to-r from-blue-500 to-blue-700 text-white font-bold rounded-lg hover:shadow-md transition-transform transform-gpu hover:-translate-y-1"
+          >
+            Entrar
+          </button>
+        </form>
+      </div>
     </div>
-    <div className="hidden md:flex justify-center items-center">
-      <img src={illustration} className='max-w-full sm:w-2/3 md:w-max ' alt="Ilustração de ferramentas de vigilância" />
-    </div>
-  </section>
-  <footer className="bg-stone-800 p-4 border-t border-stone-600">
-    <div className="text-gray-400 text-sm text-right">
-      <p>SegCode Sistema: v3.0</p>
-    </div>
-  </footer>
+  </div>
 </main>
+
+
   )
 }
 
