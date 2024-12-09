907e9440730d4be32d406d9ee4285f4506451a81
Felipe Araújo <felipe.araujo@tarraf.com.br>
2024-12-09 12:19:50 -0300

    correção em dispositovos moveis na visualização em horizontal

--- src/components/loginPage.tsx
+++ src/components/loginPage.tsx
@@ -4,57 +4,53 @@ import InputField from "./input";
 
 const LoginPage = () => {
   return (
-<main className="bg-blue-100 text-white w-full h-screen flex items-center justify-center">
+    <main className="bg-blue-100 text-white w-full h-screen flex items-center justify-center p-4 sm:p-6 min-h-96">
 
-  <div className="bg-white rounded-lg shadow-2xl flex items-center justify-center p-10 w-4/5 max-w-6xl h-4/6">
-    <div className="grid grid-cols-1 md:grid-cols-2 gap-10 w-full items-center">
-      <div className="sm:flex justify-center items-center">
-      <img
-        src={illustration}
-        className="hidden md:block sm:w-2/3 md:w-auto"
-        alt="Ilustração de ferramentas de vigilância"
-      />
-
-
-      </div>
-
-      <div className="p-8 w-full max-w-md transform">
-        <form className="space-y-6" aria-labelledby="login-form-title">
-          <div className="flex justify-center items-center align">
-             <img
-                src={logo}
-                className="max-w-full md:w-auto"
-                alt="SEGCODE Surveillance Tools"
-              />
-          </div>
-          <div className='px-6 py-4 flex flex-col justify-center gap-8'>
-          <InputField
-              id="usuario"
-              label="Usuário"
-              type="text"
-            />
-             <InputField
-              id="senha"
-              label="Senha"
-              type="password"
+      <div className="bg-white rounded-lg shadow-2xl flex items-center justify-center p-6 sm:p-10 w-full max-w-7xl h-3/4 landscape:px-24  landscape:p-[180px]">
+        <div className="grid grid-cols-1 md:grid-cols-2 gap-8 md:gap-16 w-full items-center">
+          <div className="hidden md:flex justify-center items-center ">
+            <img
+              src={illustration}
+              className="sm:w-3/4 md:w-auto"
+              alt="Ilustração de ferramentas de vigilância"
             />
           </div>
-           
-           
-          <button
-            type="submit"
-            className="w-full py-3 bg-gradient-to-r from-blue-500 to-blue-700 text-white font-bold rounded-lg hover:shadow-md transition-transform transform-gpu hover:-translate-y-1"
-          >
-            Entrar
-          </button>
-        </form>
+
+          <div className="p-4 sm:p-8 w-full max-w-sm sm:max-w-md flex-grow">
+            <form className="space-y-6" aria-labelledby="login-form-title">
+              <div className="flex justify-center items-center mb-4">
+                <img
+                  src={logo}
+                  className="max-w-full w-auto max-h-16 md:max-h-32"
+                  alt="SEGCODE Surveillance Tools"
+                />
+              </div>
+
+              <div className="px-4 sm:px-6 py-4 flex flex-col justify-center gap-6">
+                <InputField
+                  id="usuario"
+                  label="Usuário"
+                  type="text"
+                />
+                <InputField
+                  id="senha"
+                  label="Senha"
+                  type="password"
+                />
+              </div>
+
+              <button
+                type="submit"
+                className="w-full py-3 bg-gradient-to-r from-blue-500 to-blue-700 text-white font-bold rounded-lg hover:shadow-md transition-transform transform-gpu hover:-translate-y-1 text-sm sm:text-base"
+              >
+                Entrar
+              </button>
+            </form>
+          </div>
+        </div>
       </div>
-    </div>
-  </div>
-</main>
+    </main>
+  );
+};
 
-
-  )
-}
-
-export default LoginPage
+export default LoginPage;
