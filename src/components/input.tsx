999bdea8f299385346e9290ecd86688e93f2fac8
Felipe Silva <fela.19silva@gmail.com>
2024-12-11 19:39:39 -0300

    alteração background do formulario

--- src/components/input.tsx
+++ src/components/input.tsx
@@ -1,19 +1,23 @@
-const InputField = ({ id, label, type }:{id:string, label:string,type:string}) => (
-    <div className="relative">
-      <input
-        id={id}
-        name={id}
-        type={type}
-        className="peer h-10 w-full border-b border-gray-300 text-gray-900 placeholder-transparent focus:outline-none focus:border-blue-400"
-        placeholder=""
-      />
-      <label
-        htmlFor={id}
-        className="absolute left-0 -top-3.5 text-gray-600 text-sm transition-all peer-placeholder-shown:text-base peer-placeholder-shown:text-gray-400 
+const InputField = ({
+	id,
+	label,
+	type,
+}: { id: string; label: string; type: string }) => (
+	<div className="relative">
+		<input
+			id={id}
+			name={id}
+			type={type}
+			className="peer h-10 w-full border-b bg-transparent  border-gray-300 text-gray-900 placeholder-transparent focus:outline-none focus:border-blue-400"
+			placeholder=""
+		/>
+		<label
+			htmlFor={id}
+			className="absolute left-0 -top-3.5 text-gray-600 text-sm transition-all peer-placeholder-shown:text-base peer-placeholder-shown:text-gray-500 
           peer-placeholder-shown:top-2 peer-focus:-top-3.5 peer-focus:text-blue-400 peer-focus:text-sm"
-      >
-        {label}
-      </label>
-    </div>
-  );
-  export default InputField;
+		>
+			{label}
+		</label>
+	</div>
+);
+export default InputField;
--- src/components/loginPage.tsx
+++ src/components/loginPage.tsx
@@ -1,56 +1,47 @@
 import illustration from "./../assets/illustration.svg";
-import logo from './../assets/mainLogo.svg';
+import logo from "./../assets/mainLogo.svg";
 import InputField from "./input";
 
 const LoginPage = () => {
-  return (
-    <main className="bg-blue-100 text-white w-full h-screen flex items-center justify-center p-4 sm:p-6 min-h-96">
+	return (
+		<main className="bg-blue-100 text-white w-full h-screen flex items-center justify-center p-4 sm:p-6 min-h-96">
+			<div className="bg-white rounded-lg shadow-2xl flex items-center justify-center p-6 sm:p-10 w-full max-w-7xl h-3/4 landscape:px-24  landscape:p-[180px]">
+				<div className="grid grid-cols-1 md:grid-cols-2 gap-8 md:gap-16 w-full items-center">
+					<div className="hidden md:flex justify-center items-center ">
+						<img
+							src={illustration}
+							className="sm:w-3/4 md:w-auto"
+							alt="Ilustração de ferramentas de vigilância"
+						/>
+					</div>
 
-      <div className="bg-white rounded-lg shadow-2xl flex items-center justify-center p-6 sm:p-10 w-full max-w-7xl h-3/4 landscape:px-24  landscape:p-[180px]">
-        <div className="grid grid-cols-1 md:grid-cols-2 gap-8 md:gap-16 w-full items-center">
-          <div className="hidden md:flex justify-center items-center ">
-            <img
-              src={illustration}
-              className="sm:w-3/4 md:w-auto"
-              alt="Ilustração de ferramentas de vigilância"
-            />
-          </div>
+					<div className="p-4 sm:p-8 w-full max-w-sm sm:max-w-md flex-grow bg-blue-100 rounded-lg shadow-xl">
+						<form className="space-y-6" aria-labelledby="login-form-title">
+							<div className="flex justify-center items-center mb-4">
+								<img
+									src={logo}
+									className="max-w-full w-auto max-h-16 md:max-h-32"
+									alt="SEGCODE Surveillance Tools"
+								/>
+							</div>
 
-          <div className="p-4 sm:p-8 w-full max-w-sm sm:max-w-md flex-grow">
-            <form className="space-y-6" aria-labelledby="login-form-title">
-              <div className="flex justify-center items-center mb-4">
-                <img
-                  src={logo}
-                  className="max-w-full w-auto max-h-16 md:max-h-32"
-                  alt="SEGCODE Surveillance Tools"
-                />
-              </div>
+							<div className="px-4 sm:px-6 py-4 flex flex-col justify-center gap-6">
+								<InputField id="usuario" label="Usuário" type="text" />
+								<InputField id="senha" label="Senha" type="password" />
+							</div>
 
-              <div className="px-4 sm:px-6 py-4 flex flex-col justify-center gap-6">
-                <InputField
-                  id="usuario"
-                  label="Usuário"
-                  type="text"
-                />
-                <InputField
-                  id="senha"
-                  label="Senha"
-                  type="password"
-                />
-              </div>
-
-              <button
-                type="submit"
-                className="w-full py-3 bg-gradient-to-r from-blue-500 to-blue-700 text-white font-bold rounded-lg hover:shadow-md transition-transform transform-gpu hover:-translate-y-1 text-sm sm:text-base"
-              >
-                Entrar
-              </button>
-            </form>
-          </div>
-        </div>
-      </div>
-    </main>
-  );
+							<button
+								type="submit"
+								className="w-full py-3 bg-gradient-to-r from-blue-500 to-blue-700 text-white font-bold rounded-lg hover:shadow-md transition-transform transform-gpu hover:-translate-y-1 text-sm sm:text-base"
+							>
+								Entrar
+							</button>
+						</form>
+					</div>
+				</div>
+			</div>
+		</main>
+	);
 };
 
 export default LoginPage;
